dfbcbbe5bf3d2a7ba20cf322c361df3b6ff91045
stuart.stanfield <stuart.stanfield@c1919cf4-7543-11de-a486-d7ab877fdae5>
2009-08-15 23:25:31 +0000

    First commit of Sims3Pack Multi Installer

---
+++ Sims3Pack Multi Installer/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Sims3Pack_Multi_Installer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FolderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.ProgressBar1 = new System.Windows.Forms.ProgressBar();
+            this.Button2 = new System.Windows.Forms.Button();
+            this.GroupBox1 = new System.Windows.Forms.GroupBox();
+            this.Button1 = new System.Windows.Forms.Button();
+            this.txtSourceFolder = new System.Windows.Forms.TextBox();
+            this.GroupBox3 = new System.Windows.Forms.GroupBox();
+            this.Button3 = new System.Windows.Forms.Button();
+            this.txtDestinationFolder = new System.Windows.Forms.TextBox();
+            this.GroupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.GroupBox1.SuspendLayout();
+            this.GroupBox3.SuspendLayout();
+            this.GroupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ProgressBar1
+            // 
+            this.ProgressBar1.Location = new System.Drawing.Point(6, 19);
+            this.ProgressBar1.Name = "ProgressBar1";
+            this.ProgressBar1.Size = new System.Drawing.Size(458, 22);
+            this.ProgressBar1.TabIndex = 4;
+            // 
+            // Button2
+            // 
+            this.Button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Button2.Location = new System.Drawing.Point(479, 19);
+            this.Button2.Name = "Button2";
+            this.Button2.Size = new System.Drawing.Size(59, 19);
+            this.Button2.TabIndex = 2;
+            this.Button2.Text = "browse";
+            this.Button2.UseVisualStyleBackColor = true;
+            this.Button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // GroupBox1
+            // 
+            this.GroupBox1.Controls.Add(this.Button1);
+            this.GroupBox1.Controls.Add(this.txtSourceFolder);
+            this.GroupBox1.Location = new System.Drawing.Point(12, 12);
+            this.GroupBox1.Name = "GroupBox1";
+            this.GroupBox1.Size = new System.Drawing.Size(544, 50);
+            this.GroupBox1.TabIndex = 4;
+            this.GroupBox1.TabStop = false;
+            this.GroupBox1.Text = "Step 1: Select Folder that contains Sims3Packs:";
+            // 
+            // Button1
+            // 
+            this.Button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Button1.Location = new System.Drawing.Point(477, 19);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(59, 19);
+            this.Button1.TabIndex = 1;
+            this.Button1.Text = "browse";
+            this.Button1.UseVisualStyleBackColor = true;
+            this.Button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // txtSourceFolder
+            // 
+            this.txtSourceFolder.Enabled = false;
+            this.txtSourceFolder.Location = new System.Drawing.Point(10, 19);
+            this.txtSourceFolder.Name = "txtSourceFolder";
+            this.txtSourceFolder.Size = new System.Drawing.Size(461, 20);
+            this.txtSourceFolder.TabIndex = 0;
+            // 
+            // GroupBox3
+            // 
+            this.GroupBox3.Controls.Add(this.Button3);
+            this.GroupBox3.Controls.Add(this.ProgressBar1);
+            this.GroupBox3.Location = new System.Drawing.Point(12, 124);
+            this.GroupBox3.Name = "GroupBox3";
+            this.GroupBox3.Size = new System.Drawing.Size(544, 50);
+            this.GroupBox3.TabIndex = 6;
+            this.GroupBox3.TabStop = false;
+            this.GroupBox3.Text = "Step 3: Click Install:";
+            // 
+            // Button3
+            // 
+            this.Button3.Location = new System.Drawing.Point(470, 19);
+            this.Button3.Name = "Button3";
+            this.Button3.Size = new System.Drawing.Size(68, 21);
+            this.Button3.TabIndex = 5;
+            this.Button3.Text = "Install";
+            this.Button3.UseVisualStyleBackColor = true;
+            this.Button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // txtDestinationFolder
+            // 
+            this.txtDestinationFolder.Enabled = false;
+            this.txtDestinationFolder.Location = new System.Drawing.Point(11, 19);
+            this.txtDestinationFolder.Name = "txtDestinationFolder";
+            this.txtDestinationFolder.Size = new System.Drawing.Size(462, 20);
+            this.txtDestinationFolder.TabIndex = 1;
+            // 
+            // GroupBox2
+            // 
+            this.GroupBox2.Controls.Add(this.Button2);
+            this.GroupBox2.Controls.Add(this.txtDestinationFolder);
+            this.GroupBox2.Location = new System.Drawing.Point(11, 68);
+            this.GroupBox2.Name = "GroupBox2";
+            this.GroupBox2.Size = new System.Drawing.Size(544, 50);
+            this.GroupBox2.TabIndex = 5;
+            this.GroupBox2.TabStop = false;
+            this.GroupBox2.Text = "Step 2: Select Destination folder to install to:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 182);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 7;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 180);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(130, 17);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "Rename files on Save";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(569, 185);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.GroupBox1);
+            this.Controls.Add(this.GroupBox3);
+            this.Controls.Add(this.GroupBox2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Multi Sims3Pack Installer by Delphy";
+            this.GroupBox1.ResumeLayout(false);
+            this.GroupBox1.PerformLayout();
+            this.GroupBox3.ResumeLayout(false);
+            this.GroupBox2.ResumeLayout(false);
+            this.GroupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.FolderBrowserDialog FolderBrowserDialog1;
+        internal System.Windows.Forms.ProgressBar ProgressBar1;
+        internal System.Windows.Forms.Button Button2;
+        internal System.Windows.Forms.GroupBox GroupBox1;
+        internal System.Windows.Forms.Button Button1;
+        internal System.Windows.Forms.TextBox txtSourceFolder;
+        internal System.Windows.Forms.GroupBox GroupBox3;
+        internal System.Windows.Forms.Button Button3;
+        internal System.Windows.Forms.TextBox txtDestinationFolder;
+        internal System.Windows.Forms.GroupBox GroupBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+
+    }
+}
+
---
+++ Sims3Pack Multi Installer/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Windows.Forms;
+
+using System.IO;
+using System.Collections;
+using Microsoft.Win32;
+
+namespace Sims3Pack_Multi_Installer
+{
+    public partial class Form1 : Form
+    {
+        private Stack fileList = new Stack();
+        private int filesToProcess;
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Check for registry key
+            RegistryKey key = Registry.CurrentUser.OpenSubKey("Software\\Mad Scientist Productions\\Sims3PackMultiInstaller", true);
+            if (key == null)
+            {
+                key = Registry.CurrentUser.CreateSubKey("Software\\Mad Scientist Productions\\Sims3PackMultiInstaller");
+            }
+
+            if (key.GetValue("sourceFolder") != null)
+            {
+                txtSourceFolder.Text = key.GetValue("sourceFolder").ToString();
+            }
+            else
+            {
+                key.SetValue("sourceFolder", "");
+            }
+            if (key.GetValue("destFolder") != null)
+            {
+                txtDestinationFolder.Text = key.GetValue("destFolder").ToString();
+            }
+            else
+            {
+                key.SetValue("destFolder", "");
+            }
+
+            key.Close();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog1.Description = "Select the folder that contains your downloaded Sims3Pack files...";
+            if (txtSourceFolder.Text != "") { FolderBrowserDialog1.SelectedPath = txtSourceFolder.Text; }
+            FolderBrowserDialog1.ShowDialog();
+            if (FolderBrowserDialog1.SelectedPath != "") {
+                txtSourceFolder.Text = FolderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog1.Description = "Select the folder where you want to place the files extracted from the Sims3Packs...";
+            if (txtDestinationFolder.Text != "") { FolderBrowserDialog1.SelectedPath = txtDestinationFolder.Text; }
+            FolderBrowserDialog1.ShowDialog();
+            if (FolderBrowserDialog1.SelectedPath != "")
+            {
+                txtDestinationFolder.Text = FolderBrowserDialog1.SelectedPath;
+            }
+
+        }
+
+        public void ProcessFiles()
+        {
+            ProgressBar1.Minimum = 0;
+
+            DateTime start = new DateTime();
+            start = DateTime.Now;
+
+            int numFiles = this.fileList.Count;
+            string filename = "";
+
+            ProgressBar1.Maximum = numFiles + 1;
+
+            for (int i = 0; i < numFiles; i++)
+            {
+                try
+                {
+                    filename = this.fileList.Pop().ToString();
+
+                    Sims3Pack s3pack = new Sims3Pack();
+                    if (s3pack.load(filename) == true)
+                    {
+                        // Okay so now we should have a list of Sims3PackFileInfo stuff in the packagedFiles Array
+                        //Sims3PackFileInfo s3fileinfo = new Sims3PackFileInfo();
+                        for (int j = 0; j < s3pack.packagedFiles.Count; j++)
+                        {
+                            Sims3PackFileInfo s3fileinfo = (Sims3PackFileInfo)s3pack.packagedFiles[j];
+                            // Only install .package files
+                            string extension = s3fileinfo.name.Substring(s3fileinfo.name.LastIndexOf("."));
+                            //Console.WriteLine(s3fileinfo.name + " " + extension);
+                            if (extension.ToLower() == ".package")
+                            {
+                                if (s3pack.extractFile(s3fileinfo, txtDestinationFolder.Text, checkBox1.Checked) == true)
+                                {
+
+                                }
+                            }
+                        }
+                        label1.Text = "Extracted " + filename;
+                        label1.Refresh();
+                        ProgressBar1.Value++;
+
+                        
+                    }
+                    s3pack = null;
+
+                }
+                catch (System.Exception excpt)
+                {
+                    MessageBox.Show(excpt.Message + " " + excpt.StackTrace);
+                }
+            }
+
+            DateTime stop = new DateTime();
+            stop = DateTime.Now;
+            int timeTaken = (stop.Second - start.Second);
+            if (timeTaken == 0) timeTaken++;
+
+            label1.Text  = "Done " + numFiles + " files in " + timeTaken + " seconds with " + (numFiles / timeTaken) + " files per second.";
+            ProgressBar1.Value = 0;
+
+        }
+
+        public void CountFiles(string sDir)
+        {
+            try
+            {
+                /*
+                foreach (string d in Directory.GetDirectories(sDir))
+                {
+                    CountFiles(d);
+                }
+                */
+
+                DirectoryInfo dir = new DirectoryInfo(sDir);
+                FileInfo[] myFiles = dir.GetFiles("*.Sims3Pack");
+                filesToProcess += myFiles.Length;
+                foreach (FileInfo f in myFiles)
+                {
+                    this.fileList.Push(f.FullName);
+                }
+
+                Application.DoEvents();
+
+            }
+            catch (System.Exception excpt)
+            {
+                Console.WriteLine(excpt.Message);
+            }
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            if ((txtSourceFolder.Text != "") && (txtDestinationFolder.Text != ""))
+            {
+                // Save folders in registry
+                RegistryKey key = Registry.CurrentUser.OpenSubKey("Software\\Mad Scientist Productions\\Sims3PackMultiInstaller", true);
+                key.SetValue("sourceFolder", txtSourceFolder.Text);
+                key.SetValue("destFolder", txtDestinationFolder.Text);
+                key.Close();
+
+                CountFiles(txtSourceFolder.Text);
+                if (this.fileList.Count > 0)
+                {
+                    ProcessFiles();
+                }
+                else
+                {
+                    label1.Text = "No Sims3Packs found in folder.";
+                }
+            }
+        }
+
+    }
+}
