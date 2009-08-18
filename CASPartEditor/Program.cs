c5dd3a026d200658eac1c7dc2e449ff1941719ba
haasjona <haasjona@c1919cf4-7543-11de-a486-d7ab877fdae5>
2009-08-18 10:30:43 +0000

    Color picker glitch fix. And some other minor changes.

--- CASPartEditor/Program.cs
+++ CASPartEditor/Program.cs
@@ -14,7 +14,7 @@ namespace CASPartEditor
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-           Application.Run(new Form1());
+            Application.Run(new Form1());
         }
     }
 }
