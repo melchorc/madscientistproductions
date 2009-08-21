51b53d605e1445ca0b336797db0708d245d617fb
stuart.stanfield <stuart.stanfield@c1919cf4-7543-11de-a486-d7ab877fdae5>
2009-08-21 13:14:11 +0000

    - Add RenderStub

---
+++ RenderStub/Render.Designer.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MadScience.Render
+{
+    partial class Render
+    {
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.renderModeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.solidToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.wireframeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.solidWireframeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resetViewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.renderWindow1 = new MadScience.Render.RenderWindow();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.Menu;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.menuStrip1.Size = new System.Drawing.Size(753, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolsToolStripMenuItem
+            // 
+            this.toolsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.renderModeToolStripMenuItem,
+            this.resetViewToolStripMenuItem});
+            this.toolsToolStripMenuItem.Name = "toolsToolStripMenuItem";
+            this.toolsToolStripMenuItem.Size = new System.Drawing.Size(41, 20);
+            this.toolsToolStripMenuItem.Text = "View";
+            this.toolsToolStripMenuItem.Click += new System.EventHandler(this.toolsToolStripMenuItem_Click);
+            // 
+            // renderModeToolStripMenuItem
+            // 
+            this.renderModeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.solidToolStripMenuItem,
+            this.wireframeToolStripMenuItem,
+            this.solidWireframeToolStripMenuItem});
+            this.renderModeToolStripMenuItem.Name = "renderModeToolStripMenuItem";
+            this.renderModeToolStripMenuItem.Size = new System.Drawing.Size(138, 22);
+            this.renderModeToolStripMenuItem.Text = "Render Mode";
+            // 
+            // solidToolStripMenuItem
+            // 
+            this.solidToolStripMenuItem.Checked = true;
+            this.solidToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.solidToolStripMenuItem.Name = "solidToolStripMenuItem";
+            this.solidToolStripMenuItem.Size = new System.Drawing.Size(154, 22);
+            this.solidToolStripMenuItem.Text = "Solid";
+            this.solidToolStripMenuItem.Click += new System.EventHandler(this.solidToolStripMenuItem_Click);
+            // 
+            // wireframeToolStripMenuItem
+            // 
+            this.wireframeToolStripMenuItem.Name = "wireframeToolStripMenuItem";
+            this.wireframeToolStripMenuItem.Size = new System.Drawing.Size(154, 22);
+            this.wireframeToolStripMenuItem.Text = "Wireframe";
+            this.wireframeToolStripMenuItem.Click += new System.EventHandler(this.wireframeToolStripMenuItem_Click);
+            // 
+            // solidWireframeToolStripMenuItem
+            // 
+            this.solidWireframeToolStripMenuItem.Name = "solidWireframeToolStripMenuItem";
+            this.solidWireframeToolStripMenuItem.Size = new System.Drawing.Size(154, 22);
+            this.solidWireframeToolStripMenuItem.Text = "Solid+Wireframe";
+            this.solidWireframeToolStripMenuItem.Click += new System.EventHandler(this.solidWireframeToolStripMenuItem_Click);
+            // 
+            // resetViewToolStripMenuItem
+            // 
+            this.resetViewToolStripMenuItem.Name = "resetViewToolStripMenuItem";
+            this.resetViewToolStripMenuItem.Size = new System.Drawing.Size(138, 22);
+            this.resetViewToolStripMenuItem.Text = "Reset View";
+            this.resetViewToolStripMenuItem.Click += new System.EventHandler(this.resetViewToolStripMenuItem_Click);
+            // 
+            // renderWindow1
+            // 
+            //this.renderWindow1.BackgroundColour = System.Drawing.Color.Empty;
+            this.renderWindow1.CurrentFillMode = 1;
+            this.renderWindow1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.renderWindow1.Location = new System.Drawing.Point(0, 24);
+            this.renderWindow1.Name = "renderWindow1";
+            this.renderWindow1.RenderEnabled = false;
+            this.renderWindow1.Size = new System.Drawing.Size(753, 533);
+            this.renderWindow1.TabIndex = 1;
+            this.renderWindow1.WireframeColour = System.Drawing.Color.Empty;
+            // 
+            // Render
+            // 
+            this.ClientSize = new System.Drawing.Size(753, 557);
+            this.Controls.Add(this.renderWindow1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Render";
+            this.Load += new System.EventHandler(this.Render_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private RenderWindow renderWindow1;
+        private System.Windows.Forms.ToolStripMenuItem solidWireframeToolStripMenuItem;
+
+    }
+}
---
+++ RenderStub/Render.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+
+namespace MadScience.Render
+{
+    public partial class Render : System.Windows.Forms.Form
+    {
+        public Render()
+        {
+            logMessageToFile("Initalising components...");
+            InitializeComponent();
+        }
+
+        private void logMessageToFile(string message)
+        {
+            System.IO.StreamWriter sw = System.IO.File.AppendText(Path.Combine(Application.StartupPath, "renderWindow.log"));
+            try
+            {
+                string logLine = System.String.Format(
+                    "{0:G}: {1}.", System.DateTime.Now, message);
+                sw.WriteLine(logLine);
+            }
+            finally
+            {
+                sw.Close();
+            }
+        }
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem toolsToolStripMenuItem;
+        private ToolStripMenuItem renderModeToolStripMenuItem;
+        private ToolStripMenuItem solidToolStripMenuItem;
+        private ToolStripMenuItem wireframeToolStripMenuItem;
+        private ToolStripMenuItem resetViewToolStripMenuItem;
+
+        static void Main()
+        {
+            using (Render frm = new Render())
+            {
+                Application.Run(frm);
+            }
+        }
+
+
+        private void solidToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            renderWindow1.CurrentFillMode = 1;
+            solidToolStripMenuItem.Checked = true;
+            wireframeToolStripMenuItem.Checked = false;
+            solidWireframeToolStripMenuItem.Checked = false;
+        }
+
+        private void wireframeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            renderWindow1.CurrentFillMode = 0;
+            solidToolStripMenuItem.Checked = false;
+            wireframeToolStripMenuItem.Checked = true;
+            solidWireframeToolStripMenuItem.Checked = false;
+        }
+
+        private void toolsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void resetViewToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            renderWindow1.ResetView();
+        }
+
+        private void solidWireframeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            renderWindow1.CurrentFillMode = 2;
+            solidToolStripMenuItem.Checked = false;
+            wireframeToolStripMenuItem.Checked = false;
+            solidWireframeToolStripMenuItem.Checked = true;
+        }
+
+        private void Render_Load(object sender, EventArgs e)
+        {
+            if (Environment.GetCommandLineArgs().Length > 1)
+            {
+                //MessageBox.Show(Environment.GetCommandLineArgs()[0] + " " + Environment.GetCommandLineArgs()[1]);
+                Stream meshStream = File.OpenRead(Environment.GetCommandLineArgs()[1].ToString());
+                MadScience.Render.modelInfo newModel = MadScience.Render.Helpers.geomToModel(meshStream);
+                meshStream.Close();
+
+                renderWindow1.statusLabel.Text = "Loaded " + Environment.GetCommandLineArgs()[1].ToString();
+                renderWindow1.setModel(newModel);
+                renderWindow1.resetDevice();
+                renderWindow1.RenderEnabled = true;
+            }
+            else
+            {
+                logMessageToFile("RenderEnabled: true");
+                renderWindow1.RenderEnabled = true;
+                logMessageToFile("loadDefaultTextures");
+                //renderWindow1.loadDefaultTextures();
+                renderWindow1.resetDevice();
+            }
+        }
+    }
+
+}
