5c20ad1719fba5576e770b150e9baf09826cff4a
stuart.stanfield <stuart.stanfield@c1919cf4-7543-11de-a486-d7ab877fdae5>
2009-08-21 13:14:11 +0000

    - Add RenderStub

---
+++ trunk/RenderStub/Helpers.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace MadScience.Render
+{
+    public class Helpers
+    {
+
+        private static List<vertexFormat> vertexFormats = new List<vertexFormat>();
+
+        public static modelInfo geomToModel(Stream input)
+        {
+
+            modelInfo model = new modelInfo();
+            return model;
+        }
+
+    }
+
+    class vertexFormat
+    {
+        public uint dataType;
+        public uint subType;
+        public byte bytesPerElement;
+    }
+
+    public struct vertex
+    {
+        public float x;
+        public float y;
+        public float z;
+
+        public float tu;
+        public float tv;
+
+        public float bnx;
+        public float bny;
+        public float bnz;
+
+        public float tx;
+        public float ty;
+        public float tz;
+
+        public float nx;
+        public float ny;
+        public float nz;
+
+        public vertex(float newx, float newy, float newz)
+        {
+            x = newx;
+            y = newy;
+            z = newz;
+            nx = 0;
+            ny = 1;
+            nz = 0;
+            tu = 0;
+            tv = 0;
+            bnx = 0;
+            bny = 0;
+            bnz = 1;
+            tx = 1;
+            ty = 0;
+            tz = 1;
+        }
+        public vertex(float newx, float newy, float newz, float nu, float nv)
+        {
+            x = newx;
+            y = newy;
+            z = newz;
+            nx = 0;
+            ny = 1;
+            nz = 0;
+            tu = nu;
+            tv = nv;
+            bnx = 0;
+            bny = 0;
+            bnz = 1;
+            tx = 1;
+            ty = 0;
+            tz = 1;
+        }
+
+        public vertex(float newx, float newy, float newz, float nu, float nv, float nnx, float nny, float nnz)
+        {
+            x = newx;
+            y = newy;
+            z = newz;
+            nx = nnx;
+            ny = nny;
+            nz = nnz;
+            tu = nu;
+            tv = nv;
+            bnx = 0;
+            bny = 0;
+            bnz = 1;
+            tx = 1;
+            ty = 0;
+            tz = 1;
+        }
+
+        public vertex(float newx, float newy, float newz, float nu, float nv, float nnx, float nny, float nnz, float tnx, float tny, float tnz)
+        {
+            x = newx;
+            y = newy;
+            z = newz;
+            nx = nnx;
+            ny = nny;
+            nz = nnz;
+            tu = nu;
+            tv = nv;
+            tx = tnx;
+            ty = tny;
+            tz = tnz;
+            // Calculate binormal based on the cross product of the normal and tangent
+            // Should really be saving the sign of the cross product here
+            bnx = 0f;
+            bny = 0f;
+            bnz = 0f;
+        }
+
+        public static readonly int SizeInBytes = 56;
+    };
+
+    public class polygon
+    {
+        uint v1 = 0;
+        uint v2 = 0;
+        uint v3 = 0;
+        short n1 = 0;
+        short n2 = 0;
+        short n3 = 0;
+        short u1 = 0;
+        short u2 = 0;
+        short u3 = 0;
+
+        public polygon(uint nv1, uint nv2, uint nv3)
+        {
+            this.v1 = nv1;
+            this.v2 = nv2;
+            this.v3 = nv3;
+        }
+
+    }
+
+    public class uvcoord
+    {
+        public float u = 0f;
+        public float v = 0f;
+
+        public uvcoord(float nu, float nv)
+        {
+            this.u = nu;
+            this.v = nv;
+        }
+    }
+
+    public class modelInfo
+    {
+
+        public string name = "";
+        public uint numVertices = 0;
+
+        //public vertex[] vertexList;
+        //public List<Vector3> vertexList = new List<Vector3>();
+
+        //public boundingBoxes bounds = new boundingBoxes();
+
+        //public vertex[] vertexList;
+        public uint numVertexNormals = 0;
+        //public List<Vector3> vertexNormal = new List<Vector3>();
+        //public List<Vector3> vertexTangentList = new List<Vector3>();
+        //public List<Vector3> vertexBiNormalList = new List<Vector3>();
+        //public vertex[] vertexNormal;
+        public uint numUVs = 0;
+        public List<uvcoord> textureVertex = new List<uvcoord>();
+        //public uvcoord[] textureVertex;
+        public uint numPolygons = 0;
+        
+        //public polygon[] faces;
+        public bool visible = true;
+
+        //public Vector3 midPoint = new Vector3();
+
+        public List<uint> faceData = new List<uint>();
+        public List<vertex> vertexData = new List<vertex>();
+
+        //public List<polygon> faces = new List<polygon>();
+
+        //public modelTextures textures = new modelTextures();
+
+
+    }
+
+}
---
+++ trunk/RenderStub/RenderWindow.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace MadScience.Render
+{
+    public partial class RenderWindow : UserControl
+    {
+
+        public event EventHandler RequireNewTextures;
+
+        public RenderWindow()
+        {
+            this.ClientSize = new Size(640, 480);
+            //this.Text = "Direct3D (DX9/C#) - Indexed Geometry";
+            this.SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.Opaque, true);
+            InitializeComponent();
+        }
+
+        private Color backgroundColour = Color.SlateBlue;
+        [System.ComponentModel.Browsable(false)]
+        public Color BackgroundColour
+        {
+            get
+            {
+                return backgroundColour;
+            }
+            set
+            {
+                backgroundColour = value;
+            }
+        }
+        private Color wireframeColour = new Color();
+        [System.ComponentModel.Browsable(false)]
+        public Color WireframeColour
+        {
+            get
+            {
+                return wireframeColour;
+            }
+            set
+            {
+                wireframeColour = value;
+            }
+        }
+
+
+        private bool renderEnabled;
+        [System.ComponentModel.Browsable(false)]
+        public bool RenderEnabled
+        {
+            get
+            {
+                return renderEnabled;
+            }
+            set
+            {
+                renderEnabled = value;
+                if (renderEnabled)
+                    Invalidate();
+            }
+        }
+
+        private int mousing = 0;
+        public Label statusLabel;
+        public Label lblGeneratingTexture;
+        //private System.ComponentModel.IContainer components;
+
+        private int fillMode = 1;
+        [System.ComponentModel.Browsable(false)]
+        public int CurrentFillMode
+        {
+            get
+            {
+                return fillMode;
+            }
+            set
+            {
+                fillMode = value;
+                Invalidate();
+            }
+        }
+
+        public int shaderMode = 0;
+
+        public void setModel(modelInfo newModel)
+        {
+        }
+
+        protected override void OnPaint(System.Windows.Forms.PaintEventArgs e)
+        {
+            base.OnPaint(e);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
+
+        private void Init()
+        {
+        }
+
+        private void CancelResize(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (Width < 1 || Height < 1)
+                e.Cancel = true;
+        }
+
+        public void OnLostDevice(object sender, EventArgs e)
+        {
+        }
+
+        public void DeInit()
+        {
+            renderEnabled = false;
+            statusLabel.Text = "3d view currently not initialised.";
+        }
+
+        private void setupShaders()
+        {
+        }
+
+        public void resetDevice()
+        {
+        }
+
+        /// <summary>
+        /// This event-handler is a good place to create and initialize any 
+        /// Direct3D related objects, which may become invalid during a 
+        /// device reset.
+        /// </summary>
+        public void OnResetDevice(object sender, EventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// This method is dedicated completely to rendering our 3D scene and is
+        /// is called by the OnPaint() event-handler.
+        /// </summary>
+        private void Render()
+        {
+        }
+
+        public void loadTexture(Stream textureInput, string outputTexture)
+        {
+        }
+
+        public void loadTextureFromBitmap(Bitmap textureInput, string outputTexture)
+        {
+        }
+
+        public void loadDefaultTextures()
+        {
+        }
+
+        protected override void OnEnter(EventArgs e)
+        {
+            base.OnEnter(e);
+        }
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            base.OnMouseMove(e);
+        }
+
+        protected override void OnMouseWheel(MouseEventArgs e)
+        {
+            base.OnMouseWheel(e);
+        }
+
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+             base.OnKeyDown(e);
+        }
+
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            base.OnMouseDown(e);
+        }
+
+        protected override void OnMouseUp(MouseEventArgs e)
+        {
+            base.OnMouseUp(e);
+        }
+
+        protected override void OnMouseClick(MouseEventArgs e)
+        {
+            base.OnMouseClick(e);
+        }
+
+        public void ResetView()
+        {
+            ResetView(180);
+        }
+
+        private void ResetView(int spX)
+        {
+        }
+
+        private void InitializeComponent()
+        {
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.lblGeneratingTexture = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.BackColor = System.Drawing.SystemColors.Menu;
+            this.statusLabel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.statusLabel.Location = new System.Drawing.Point(318, 0);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(153, 13);
+            this.statusLabel.TabIndex = 1;
+            this.statusLabel.Text = "3d view currently not initialised.";
+            this.statusLabel.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            // 
+            // lblGeneratingTexture
+            // 
+            this.lblGeneratingTexture.AutoSize = true;
+            this.lblGeneratingTexture.BackColor = System.Drawing.SystemColors.Menu;
+            this.lblGeneratingTexture.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lblGeneratingTexture.Location = new System.Drawing.Point(0, 233);
+            this.lblGeneratingTexture.Name = "lblGeneratingTexture";
+            this.lblGeneratingTexture.Size = new System.Drawing.Size(103, 13);
+            this.lblGeneratingTexture.TabIndex = 2;
+            this.lblGeneratingTexture.Text = "Generating texture...";
+            this.lblGeneratingTexture.Visible = false;
+            // 
+            // RenderWindow
+            // 
+            this.Controls.Add(this.lblGeneratingTexture);
+            this.Controls.Add(this.statusLabel);
+            this.Name = "RenderWindow";
+            this.Size = new System.Drawing.Size(471, 246);
+            this.Load += new System.EventHandler(this.RenderWindow_Load);
+            this.VisibleChanged += new System.EventHandler(this.RenderWindow_VisibleChanged);
+            this.Resize += new System.EventHandler(this.RenderWindow_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void RenderWindow_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void frontToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ResetView(180);
+        }
+
+        private void backToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ResetView(0);
+        }
+
+        private void leftSideToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ResetView(90);
+        }
+
+        private void rightToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ResetView(270);
+        }
+
+        private void RenderWindow_Resize(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RenderWindow_VisibleChanged(object sender, EventArgs e)
+        {
+            Console.WriteLine("Test2");
+        }
+
+    }
+}
