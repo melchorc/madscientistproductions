3c1b67cd5c0fbaa580e47a140b070da4dab68c45
stuart.stanfield <stuart.stanfield@c1919cf4-7543-11de-a486-d7ab877fdae5>
2009-10-06 10:23:19 +0000

    - Improved error checking in VPXY and Bone - Add extra Mesh Lods

--- trunk/MadScience.Wrappers/BoneDelta.cs
+++ trunk/MadScience.Wrappers/BoneDelta.cs
@@ -38,8 +38,8 @@ namespace MadScience.Wrappers
             }
             if (this.rcolHeader.externalChunks.Count == 0 && this.rcolHeader.internalChunks.Count == 0)
             {
-                throw new Exception("You must have a valid RCOL header");
-                //return;
+                //throw new Exception("You must have a valid RCOL header");
+                return;
             }
 
             this.rcolHeader.Save(output);
