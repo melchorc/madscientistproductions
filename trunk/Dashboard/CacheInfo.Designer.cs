935db84d590d4e0d6f4ca34751f12a632fc47e99
stuart.stanfield <stuart.stanfield@c1919cf4-7543-11de-a486-d7ab877fdae5>
2011-05-30 20:35:48 +0000

    - Split off Cache Info and Framework Info to seperate forms - Added Dump Info button to framework form

---
+++ trunk/Dashboard/CacheInfo.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Sims3Dashboard
+{
+    partial class CacheInfo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button6 = new System.Windows.Forms.Button();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.columnHeader6 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader7 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader8 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader9 = new System.Windows.Forms.ColumnHeader();
+            this.button7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button6
+            // 
+            this.button6.Enabled = false;
+            this.button6.Location = new System.Drawing.Point(1, 149);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(91, 23);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Empty Checked";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // listView2
+            // 
+            this.listView2.CheckBoxes = true;
+            this.listView2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader6,
+            this.columnHeader7,
+            this.columnHeader8,
+            this.columnHeader9});
+            this.listView2.FullRowSelect = true;
+            this.listView2.GridLines = true;
+            this.listView2.Location = new System.Drawing.Point(1, 2);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(382, 141);
+            this.listView2.TabIndex = 8;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.Details;
+            this.listView2.ItemChecked += new System.Windows.Forms.ItemCheckedEventHandler(this.listView2_ItemChecked);
+            this.listView2.ColumnClick += new System.Windows.Forms.ColumnClickEventHandler(this.listView2_ColumnClick);
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Cache Name";
+            this.columnHeader6.Width = 220;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "Filename";
+            this.columnHeader7.Width = 0;
+            // 
+            // columnHeader8
+            // 
+            this.columnHeader8.Text = "Size (KB)";
+            this.columnHeader8.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.columnHeader8.Width = 70;
+            // 
+            // columnHeader9
+            // 
+            this.columnHeader9.Text = "Entries";
+            this.columnHeader9.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(308, 149);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "Clear All";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // CacheInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(385, 177);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.button7);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "CacheInfo";
+            this.Text = "CacheInfo";
+            this.Load += new System.EventHandler(this.CacheInfo_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+        private System.Windows.Forms.ColumnHeader columnHeader8;
+        private System.Windows.Forms.ColumnHeader columnHeader9;
+        private System.Windows.Forms.Button button7;
+    }
+}
---
+++ trunk/Dashboard/CacheInfo.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Sims3Dashboard
+{
+    public partial class CacheInfo : Form
+    {
+        public CacheInfo()
+        {
+            InitializeComponent();
+        }
+
+        private void CacheInfo_Load(object sender, EventArgs e)
+        {
+            getCacheDetails();
+        }
+
+        MadScience.ListViewSorter Sorter = new MadScience.ListViewSorter();
+
+        private void getCacheDetails()
+        {
+            string TS3MyDocs = MadScience.Helpers.getRegistryValue("cacheFolders");
+            if (String.IsNullOrEmpty(TS3MyDocs))
+            {
+
+                string myDocs = MadScience.Helpers.findMyDocs();
+                if (String.IsNullOrEmpty(myDocs)) return;
+
+                TS3MyDocs = Path.Combine(Path.Combine(myDocs, "Electronic Arts"), MadScience.Helpers.gamesInstalled.baseName);
+            }
+            listView2.Items.Clear();
+
+            showCacheInfo(Path.Combine(TS3MyDocs, "CASPartCache.package"), "CASPartCache");
+            showCacheInfo(Path.Combine(TS3MyDocs, "compositorCache.package"), "compositorCache");
+            showCacheInfo(Path.Combine(TS3MyDocs, "scriptCache.package"), "scriptCache");
+            showCacheInfo(Path.Combine(TS3MyDocs, "simCompositorCache.package"), "simCompositorCache");
+
+            DirectoryInfo dir;
+            FileInfo[] myFiles;
+
+            if (Directory.Exists(Path.Combine(TS3MyDocs, "WorldCaches")))
+            {
+                dir = new DirectoryInfo(Path.Combine(TS3MyDocs, "WorldCaches"));
+                myFiles = dir.GetFiles("*.package");
+                foreach (FileInfo f in myFiles)
+                {
+                    showCacheInfo(f.FullName, "WorldCaches\\" + f.Name);
+                }
+            }
+
+            if (Directory.Exists(Path.Combine(TS3MyDocs, "Thumbnails")))
+            {
+                dir = new DirectoryInfo(Path.Combine(TS3MyDocs, "Thumbnails"));
+                myFiles = dir.GetFiles("*.package");
+                foreach (FileInfo f in myFiles)
+                {
+                    showCacheInfo(f.FullName, "Thumbnails\\" + f.Name);
+                }
+            }
+
+            //showCacheInfo("", "");
+
+        }
+
+        private void showCacheInfo(string cachePath, string cacheName)
+        {
+            if (!String.IsNullOrEmpty(cachePath) && !File.Exists(cachePath)) return;
+
+
+            // CAS Part Cache
+            ListViewItem item = new ListViewItem();
+
+            item.Text = cacheName;
+            item.SubItems.Add(cachePath);
+
+            if (!String.IsNullOrEmpty(cachePath))
+            {
+                if (!File.Exists(cachePath))
+                {
+                    item.SubItems.Add("0");
+                    item.SubItems.Add("0");
+
+                }
+                else
+                {
+
+                    FileInfo f = new FileInfo(cachePath);
+                    if (f.Length > 0)
+                    {
+                        item.SubItems.Add(((f.Length / 1024) + 1).ToString());
+                    }
+                    else
+                    {
+                        item.SubItems.Add("0");
+                    }
+
+                    Stream casPartCache = File.OpenRead(cachePath);
+                    MadScience.Wrappers.Database db = new MadScience.Wrappers.Database(casPartCache, true);
+                    item.SubItems.Add(db.dbpf.Entries.Count.ToString());
+                    casPartCache.Close();
+                }
+            }
+            else
+            {
+                item.SubItems.Add("");
+                item.SubItems.Add("");
+            }
+
+            listView2.Items.Add(item);
+        }
+
+        private void clearCache(string cachePath, string cacheName)
+        {
+            if (String.IsNullOrEmpty(cacheName))
+            {
+                // Clear ALL caches
+                if (MessageBox.Show("You are about to clear all caches.  Are you sure?", "Clear All Caches", MessageBoxButtons.YesNoCancel) == DialogResult.Yes)
+                {
+                    string myDocs = MadScience.Helpers.findMyDocs();
+                    if (String.IsNullOrEmpty(myDocs)) return;
+
+                    string TS3MyDocs = Path.Combine(Path.Combine(myDocs, "Electronic Arts"), "The Sims 3");
+
+                    DirectoryInfo dir;
+                    FileInfo[] myFiles;
+
+                    if (Directory.Exists(Path.Combine(TS3MyDocs, "WorldCaches")))
+                    {
+                        dir = new DirectoryInfo(Path.Combine(TS3MyDocs, "WorldCaches"));
+                        myFiles = dir.GetFiles("*.package");
+                        foreach (FileInfo f in myFiles)
+                        {
+                            File.Delete(f.FullName);
+                        }
+                    }
+
+                    if (Directory.Exists(Path.Combine(TS3MyDocs, "Thumbnails")))
+                    {
+                        dir = new DirectoryInfo(Path.Combine(TS3MyDocs, "Thumbnails"));
+                        myFiles = dir.GetFiles("*.package");
+                        foreach (FileInfo f in myFiles)
+                        {
+                            File.Delete(f.FullName);
+                        }
+                    }
+
+                    try
+                    {
+                        File.Delete(Path.Combine(TS3MyDocs, "CASPartCache.package"));
+                        File.Delete(Path.Combine(TS3MyDocs, "compositorCache.package"));
+                        File.Delete(Path.Combine(TS3MyDocs, "scriptCache.package"));
+                        File.Delete(Path.Combine(TS3MyDocs, "simCompositorCache.package"));
+                    }
+                    catch (Exception ex)
+                    {
+                    }
+                }
+
+            }
+            else
+            {
+                if (MessageBox.Show("You are about to delete the " + cacheName + " cache.  Are you sure?", "Clear " + cacheName, MessageBoxButtons.YesNoCancel) == DialogResult.Yes)
+                {
+                    File.Delete(cachePath);
+                }
+            }
+
+
+        }
+
+        private void listView2_ColumnClick(object sender, ColumnClickEventArgs e)
+        {
+            Sorter.Sort(listView2, e);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            clearCache("", "");
+            getCacheDetails();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < listView2.Items.Count; i++)
+            {
+                if (listView2.Items[i].Checked) clearCache(listView2.Items[i].SubItems[1].Text, listView2.Items[i].Text);
+            }
+            getCacheDetails();
+        }
+
+
+        private void listView2_ItemChecked(object sender, ItemCheckedEventArgs e)
+        {
+            int numChecked = listView2.CheckedItems.Count;
+            if (numChecked > 0)
+            {
+                button6.Enabled = true;
+            }
+            else
+            {
+                button6.Enabled = false;
+            }
+        }
+
+    }
+}
